a3782b677cf643ed6d9ecc3c0caeaf23012d4a3a
Brandon Guttersohn <bguttersohn@gmail.com>
2019-03-29 20:03:29 -0500

    Copy in newest version (March 2019)
    Copying in newest version of my-init.org from private repo. Sorry for
    the ugly diff.
    
    Further updates will likely be similar -- my personal emacs config
    repo regularly contains some personal info. I'll just be placing
    periodic sanitized snapshots here for people who are interested in my
    setup.

--- bootstrap.el
+++
@@ -1,3 +0,0 @@
-(setq custom-file (concat (file-name-directory load-file-name) "custom.el"))
-(load-file custom-file)
-(org-babel-load-file (concat (file-name-directory load-file-name) "my-init.org"))
