92b6de9ee1e35b929b65c861a536b9d71f376fc2
Brandon Guttersohn <brandon@guttersohn.org>
2026-06-21 20:49:14 -0500

    Various fixes for Emacs 30 and NixOS 26.05
    Some of these are to make my distro of emacs work on fresh machines
    missing some of my traditional sync'd files.

--- bootstrap.el
+++ bootstrap.el
@@ -1,3 +1,33 @@
+(message "SECTION: Bootstrap.")
+
+;; Load custom
 (setq custom-file "~/.emacs.d/emacs-config/custom.el")
 (load-file custom-file)
+
+;; Set up straight.el Need to do this before org-babel-load-file so
+;; that the newest org (from straight.el) is the only one loaded.
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name
+        "straight/repos/straight.el/bootstrap.el"
+        (or (bound-and-true-p straight-base-dir)
+            user-emacs-directory)))
+      (bootstrap-version 7))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(straight-use-package 'use-package)
+
+(setq straight-use-package-by-default t)
+
+(add-to-list 'load-path "~/.emacs.d/emacs-config/lisp")
+
+;; Kickoff main init
+(use-package org)
 (org-babel-load-file "~/.emacs.d/emacs-config/my-init.org")
